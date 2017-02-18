b62ee39c9d34546c988415cf2fa21a603ee9099e
Nishant Roy <nroy@gatech.edu>
2017-02-18 18:39:14 -0500

    init

---
+++ server.js
@@ -0,0 +1,21 @@
+'use strict';
+const express = require('express')
+const path = require("path");
+
+
+var port = process.env.PORT || 8080;
+const request = require('request');
+
+const bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({
+    extended: true
+})); // support encoded bodies
+
+
+app.post('/api/summaryBot', function (req, res) {
+    console.log("Called now with this body: " + JSON.stringify(req.body));
+});
